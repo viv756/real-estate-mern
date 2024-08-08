3868eb5f23e981b84db966ba1e7c323247635acf
*vivek <vrvivekofficial@gmail.com>
2024-08-08 11:44:00 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="bg-slate-200 shadow-md ">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-md sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Vivek</span>
             <span className="text-slate-700">Estate</span>
           </h1>
