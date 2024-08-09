e7f1d480ff74490754d2dc5b57ab87e7bdf72e50
*vivek <vrvivekofficial@gmail.com>
2024-08-09 23:53:02 +0530

    create and add OAuth component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
 import "./index.css";
-import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
