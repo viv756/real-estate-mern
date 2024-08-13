28fa45946897b66b5ab4471b101d48bf1de64198
*vivek <vrvivekofficial@gmail.com>
2024-08-13 19:28:08 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -10,21 +11,41 @@ export const createListing = async (req, res, next) => {
   }
 };
 
-
-export const deleteListing = async (req, res, next)=>{
-  const listing = await Listing.findById(req.params.id)
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
   if (!listing) {
-    return next(errorHandler(401,"You can only delete your own listings"))
+    return next(errorHandler(401, "You can only delete your own listings"));
   }
 
   if (req.user.id !== listing.userRef) {
-    return next(errorHandler(401,"You can only delete your own listings"))
+    return next(errorHandler(401, "You can only delete your own listings"));
   }
-  
+
   try {
-    await Listing.findByIdAndDelete(req.params.id)
-    res.status(200).json("listing has been deleted")
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("listing has been deleted");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const updateListing = async (req, res, next) => {
+
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(errorHandler(400, "Invalid ID format"));
+  }
+
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listing"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
