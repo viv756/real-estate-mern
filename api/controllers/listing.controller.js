46b686bec034369afcb63f590eaaf1f9d70e84e7
*vivek <vrvivekofficial@gmail.com>
2024-08-14 17:03:33 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -30,7 +30,6 @@ export const deleteListing = async (req, res, next) => {
 };
 
 export const updateListing = async (req, res, next) => {
-
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return next(errorHandler(400, "Invalid ID format"));
   }
@@ -50,14 +49,60 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export const getListing = async(req, res, next)=> {
+export const getListing = async (req, res, next) => {
   try {
-    const listing = await Listing.findById(req.params.id)
+    const listing = await Listing.findById(req.params.id);
     if (!listing) {
-      return next(errorHandler(404,"Listing not found"))
+      return next(errorHandler(404, "Listing not found"));
     }
-    res.status(200).json(listing)
+    res.status(200).json(listing);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const satrtIndex = parseInt(req.query.satrtIndex) || 0;
+
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(satrtIndex);
+
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
